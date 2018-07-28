f8c85619519f16eb3c0170871a839b4523fdad34
Rajas Abhyankar <rajas.devel@gmail.com>
2018-07-28 22:40:42 +0530

    Minor changes

--- macos.sh
+++ macos.sh
@@ -34,17 +34,6 @@ fi
 
 defaults write -g PMPrintingExpandedStateForPrint -bool TRUE
 
-# Enable NTFS support, not stable. TODO: test this thoroughly before uncommenting
-# https://gist.github.com/bjorgvino/f24e5c079b92f921b765
-# brew uninstall osxfuse
-# brew install Caskroom/cask/osxfuse
-# brew install homebrew/fuse/ntfs-3g
-# brew update && brew install ntfs-3g
-# sudo mv /sbin/mount_ntfs /sbin/mount_ntfs.original
-# sudo ln -s /usr/local/Cellar/ntfs-3g/2014.2.15/sbin/mount_ntfs /sbin/mount_ntfs
-# ls -l /sbin/mount_ntfs*
-# reboot
-
 
 # Install OhMyZSH
 curl -L http://install.ohmyz.sh | sh
@@ -70,101 +59,80 @@ brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
 # Brew packages
-brew install wget
-brew install openssl
-brew install python
-brew install python3
-brew install node
-brew install gcc
-# Required for numpy
-brew install gfortran
+brew install ack
 brew install Caskroom/cask/xquartz
-brew install r
+brew install ffmpeg
+brew install gcc
+brew install gfortran
+brew install htop
+brew install imagemagick
+brew install iotop
+brew install mongodb
+brew install mtr --no-gtk
+brew install mysql
+brew install nginx
+brew install nmap
+brew install node
+brew install openssl
 brew install php70 --with-pear --with-imap
 brew install php70-xdebug
-brew install nginx
 brew install postgresql
-brew install mongodb
-brew install mysql
+brew install pyqt
+brew install python
+brew install python3
+brew install r
 brew install redis
-brew install ffmpeg
-brew install imagemagick
-brew install htop
-brew install iotop
-brew install ack
+brew install wget
+brew install wireshark --with-qt
+brew install zmq
 
 # Cask packages
-brew cask install java
-brew cask install iterm2
-brew cask install opera
-brew cask install firefoxdeveloperedition
-brew cask install google-chrome
-brew cask install intellij-idea
+brew cask install anaconda
 brew cask install android-studio
-brew cask install rstudio
-brew cask install sublime-text3
 brew cask install atom
+brew cask install calibre
+brew cask install dropbox
+brew cask install firefoxdeveloperedition
+brew cask install gimp
+brew cask install google-chrome
+brew cask install inkscape
+brew cask install intellij-idea
+brew cask install iterm2
+brew cask install java
+brew cask install libreoffice
 brew cask install macvim
+brew cask install mounty
 brew cask install mysql-workbench
+brew cask install netspot
+brew cask install opera
 brew cask install pgadmin3
 brew cask install qbittorrent
-brew cask install libreoffice
-brew cask install virtualbox
-brew cask install vagrant
-brew cask install textexpander
-
+brew cask install rstudio
 brew cask install skype
+brew cask install sublime-text3
 brew cask install teamviewer
-brew cask install dropbox
+brew cask install textexpander
+brew cask install vagrant
+brew cask install virtualbox
 brew cask install vlc
-brew cask install calibre
-brew cask install gimp
-brew cask install inkscape
-
-brew install mtr --no-gtk
-brew cask install netspot
-brew install nmap
-brew install wireshark --with-qt
 
 # Python
-pip install virtualenv
-pip install virtualenvwrapper
-pip install simplejson
-pip install six
-pip install Django
-pip install Flask 
-pip install numpy
-pip install scipy
-pip install matplotlib
-pip install ipython
-brew install pyqt
-brew install zmq
-pip install pyzmq
-pip install pygments
+pip install virtualenv virtualenvwrapper simplejson six Django Flask numpy scipy matplotlib ipython pyzmq pygments
 
 # NPM packages
 npm install -g yo grunt-cli protractor karma-cli jshint jscs
-
-# Java
-
 
 # PHP
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 echo 'export PATH="$PATH:~/.composer/vendor/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:~/.composer/vendor/bin"' >> ~/.zshrc
-composer global require "phpmd/phpmd=@stable"
-composer global require "squizlabs/php_codesniffer=*"
-composer global require "phpunit/phpunit=*"
-composer global require "laravel/installer=~1.1"
 # Add composer path to profile
 
 # Ruby
 gem install rails
 gem install bropages
 
-# Sign in to essential services, TODO: How to do it from shell script?
-echo "Sign in to essential services like App Store, Dropbox, Google"
 
 # From the App Store, install:
 # xcode, evernote, dash3, skitch, pocket, mediashare, imovie, microsoft remote desktop, kindle
